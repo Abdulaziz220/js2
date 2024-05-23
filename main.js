d9a1cc451e39b8978b3a6de172d041ffa01d5e96
Your Name <you@example.com>
2024-05-23 12:54:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let name1 = prompt('Ismingizni kiriting');
+let id = prompt('Space idingizni kiriting');
+let password = prompt('Space parolingizni kiriting');
+
+const malumot = {
+    name: name1,
+    id: id,
+    password: password,
+}
+console.log(malumot);   
